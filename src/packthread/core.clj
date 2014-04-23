53683f017439cdcee37a7524b5922a64fc6cfd83
Jason Felice <jason.m.felice@gmail.com>
2014-04-23 14:01:12 -0400

    Simplify with `if-let`

--- src/packthread/core.clj
+++ src/packthread/core.clj
@@ -5,8 +5,7 @@
   [value & forms]
   (loop [forms forms
          result value]
-    (if-not (seq forms)
-      result
-      (let [[form rest-of-forms] forms]
-        (recur rest-of-forms
-               (apply list (first form) result (rest form)))))))
+    (if-let [[form rest-of-forms] forms]
+      (recur rest-of-forms
+             (apply list (first form) result (rest form)))
+      result)))
