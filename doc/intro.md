8ced0b5dcc93287fff79ffa6be420db3cdbc72b5
Jason Felice <jason.m.felice@gmail.com>
2014-04-23 13:30:14 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to packthread
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/packthread/core_test.clj
@@ -0,0 +1,7 @@
+(ns packthread.core-test
+  (:require [clojure.test :refer :all]
+            [packthread.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
