54ad6d42d66fb16824dfb6ba2f8070dc3b670fd3
Jason Felice <jason.m.felice@gmail.com>
2014-04-23 13:33:37 -0400

    Fill in README

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject packthread "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.maitria/packthread "0.1.0-SNAPSHOT"
+  :description "Replacement threading macro"
+  :url "https://github.com/maitria/packthread"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
