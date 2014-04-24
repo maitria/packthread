37050fcf0053db28ca12ce9a1ee164d441266f5c
Jason Felice <jason.m.felice@gmail.com>
2014-04-24 00:28:05 -0400

    `->` handles `if`

--- src/packthread/core.clj
+++ src/packthread/core.clj
@@ -1,9 +1,26 @@
 (ns packthread.core
   (:refer-clojure :exclude [->]))
 
-(defn- thread
+(defmulti thread (fn [value form] (first form)))
+
+(defmethod thread :default
   [value form]
   (apply list (first form) value (rest form)))
+
+(defmethod thread 'if
+  [value form]
+  (let [if-test (second form)
+        value-symbol (gensym)
+        then (nth form 2)
+        else (if (= 4 (count form))
+               (nth form 3)
+               '(identity))
+        threaded-then (thread value-symbol then)
+        threaded-else (thread value-symbol else)]
+    `(let [~value-symbol ~value]
+       (if ~if-test
+         ~threaded-then 
+         ~threaded-else))))
 
 (defmacro ->
   [value & forms]
