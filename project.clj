f52d76c77fa64ed94b8ac979c48accbd4fbcce6a
Jason Felice <jason.m.felice@gmail.com>
2014-04-30 15:12:34 -0500

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.maitria/packthread "0.1.0"
+(defproject com.maitria/packthread "0.1.1"
   :description "Threading macros for working with globs of state"
   :url "https://github.com/maitria/packthread"
   :license {:name "avi license"
