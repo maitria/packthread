43fefb2f8a0ba6ab2ec5147639886fad13942901
Jason Felice <jason.m.felice@gmail.com>
2015-01-31 19:20:46 -0500

    lein-release plugin: preparing 0.1.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.maitria/packthread "0.1.3-SNAPSHOT"
+(defproject com.maitria/packthread "0.1.3"
   :description "Threading macros for working with globs of state"
   :url "https://github.com/maitria/packthread"
   :license {:name "avi license"
